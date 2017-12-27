3f7a3967447eacfd95f4f2f25049d30f6ec59935
Jake Chavez <jake94chavez@gmail.com>
2017-12-27 13:12:27 -0600

    Submit successfully grabs the correct score from questions

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
 // Adds a click event listener to submit (doesn't actually submit anything yet)
-let onSubmit = () => {
+const onSubmit = () => {
 	$('#submission').click((event) => {
 		alert('Submitted!')
+// Get answer score
+let answerScore = (qName) => {
+	debugger;
+	let $radiosNo = $('input[name=' + qName 
+		+ ']');
+
+	for (let i=0; i < $radiosNo.length; i++) {
+		if ($radiosNo[i].checked) {
+			var answerValue = Number($radiosNo[i].value);
+		}
+	}
+	if (isNaN(answerValue)) {
+		var answerValue = 0;
+	}
+	return answerValue;
+}
+
+const getScore = (answerScore('q1') + answerScore('q2') + answerScore('q3') + 
+	answerScore('q4') + answerScore('q5') + answerScore('q6') + answerScore('q7') + 
+	answerScore('q8') + answerScore('q9') + answerScore('q10'));
+	console.log('getScore: ' + getScore);
 	});
 }
+
 
 onSubmit();
