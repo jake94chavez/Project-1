2468a8216be83c09c53c32e9e54c6baa7441986f
Jake Chavez <jake94chavez@gmail.com>
2017-12-27 15:35:32 -0600

    Quiz grabs incorrect answers, appending correct answers WIP

--- app.js
+++ app.js
@@ -1,29 +1,44 @@
 // Adds a click event listener to submit (doesn't actually submit anything yet)
 const onSubmit = () => {
 	$('#submission').click((event) => {
-		alert('Submitted!')
-// Get answer score
-let answerScore = (qName) => {
-	debugger;
-	let $radiosNo = $('input[name=' + qName 
+		console.log('Submitted!')
+	// Get individual answer score
+	let answerScore = (qName) => {
+		let $radiosNo = $('input[name=' + qName 
 		+ ']');
 
-	for (let i=0; i < $radiosNo.length; i++) {
-		if ($radiosNo[i].checked) {
-			var answerValue = Number($radiosNo[i].value);
+		for (let i = 1; i < $radiosNo.length; i++) {
+			if ($radiosNo[i].checked) {
+				var answerValue = Number($radiosNo[i].value);
+			}
 		}
+		if (isNaN(answerValue)) {
+			var answerValue = 0;
+		}
+		return answerValue;
 	}
-	if (isNaN(answerValue)) {
-		var answerValue = 0;
-	}
-	return answerValue;
-}
+	// Total Score
+	const getScore = (answerScore('q1') + answerScore('q2') + answerScore('q3') + 
+		answerScore('q4') + answerScore('q5') + answerScore('q6') + answerScore('q7') + 
+		answerScore('q8') + answerScore('q9') + answerScore('q10'));
+		console.log('getScore: ' + getScore);
 
-const getScore = (answerScore('q1') + answerScore('q2') + answerScore('q3') + 
-	answerScore('q4') + answerScore('q5') + answerScore('q6') + answerScore('q7') + 
-	answerScore('q8') + answerScore('q9') + answerScore('q10'));
-	console.log('getScore: ' + getScore);
-	});
+	// Return correct answer string
+	let correctAnswer = (correctStringNo, qNumber) => {
+		console.log('qNumber: ' + qNumber);
+		return ('The correct answer for question ' + qNumber + ' is <strong>' +
+			$('#' + correctStringNo).innerHtml + '</strong>');
+			}
+
+	// Print correct answers for wrong questions only
+		for (let i = 1; i <= 10; i++) {
+			let question = ('q' + i)
+			if (answerScore(question) === 0 ) {
+				let correct = ('#correctAnswer' + i)
+				$(correct).innerHtml = correctAnswer(('correctChoice' + i), i);
+			}
+			}
+		});
 }
 
 
